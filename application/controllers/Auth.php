c5ee0de1afbb6478b25fa4c98dc348c23906f996
acepsaepudin <acepsaepudin>
2016-07-21 15:55:56 +0700

    add validation on register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -15,7 +15,14 @@ class Auth extends CI_Controller
 	public function register()
 	{
 		if ($this->input->server('REQUEST_METHOD') == 'POST') {
-			die('adf');
+			$this->form_validation->set_rules('nama','Nama Lengkap', 'required');
+			$this->form_validation->set_rules('email','Email', 'required');
+			$this->form_validation->set_rules('alamat','Alamat', 'required');
+			$this->form_validation->set_rules('password','Password', 'required');
+			$this->form_validation->set_rules('password_confirmation','Konfirmasi Password', 'required');
+			if ($this->form_validation->run() == false) {
+				die('salah');
+			}
 		} else {
 			$this->load->view('register');
 		}
